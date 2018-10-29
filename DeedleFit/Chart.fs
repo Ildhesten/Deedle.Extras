81f864bfad1c0f2c6e0066ba72a7d72e3e551937
Ildhesten <noneofyour@business.com>
2018-10-28 20:44:00 -0400

    Initial commit

---
+++ DeedleFit/Chart.fs
@@ -0,0 +1,31 @@
+﻿namespace DeedleFit.Charting
+open Deedle
+open XPlot.Plotly
+
+type Chart() =
+  static member private LineGraph (s : Series<'a,'b>, name : string option) =
+    match name with
+    | None -> Graph.Scatter(x = s.Keys, y = s.Values, mode = "Line")
+    | Some name -> Graph.Scatter(x = s.Keys, y = s.Values, mode = "Line", name = name)
+    
+  static member Line (s : Series<'a,'b>, ?name : string) =
+    Chart.LineGraph (s,name)
+    |> Chart.Plot
+
+  static member Line (s : Series<'a,'b> seq, names : string option seq) =
+    if Seq.length s <> Seq.length names then
+      failwith "Sequence of data series must be of same length as the names"
+    let namedSeries = Seq.zip s names
+    namedSeries
+    |> Seq.map Chart.LineGraph
+    |> Seq.map (fun x -> x :> Graph.Trace)
+    |> Chart.Plot
+
+  static member Line (s : Series<'a,'b> seq, names : string seq) =
+    let mappedNames = Seq.map Some names
+    Chart.Line (s, mappedNames)
+
+  static member Line(df : Frame<'a,'b>, columnKeys : 'b seq) =
+    let names = columnKeys |> Seq.map (sprintf "%A") |> Seq.map Some
+    let series = columnKeys |> Seq.map (fun k -> df.[k])
+    Chart.Line(series, names)
---
+++ DeedleFit/LinearRegression.fsi
@@ -0,0 +1,28 @@
+﻿namespace DeedleFit
+
+module LinearRegression =
+  open Deedle
+
+  module Fit =
+    type t<'a,'b> when 'a : equality and 'b : equality
+
+    val input : t<'a,'b> -> Frame<'a,'b>
+    val coefficients : t<'a,'b> -> Series<'b, float>
+    val fitIntercept : t<'a,'b> -> 'b option
+    val fittedValues : t<'a,'b> -> Series<'a, float>
+    val residuals : t<'a,'b> -> Series<'a,float>
+
+    module Summary =
+      type t<'b> when 'b : equality =
+        {
+          LinearFormula : string
+          ResidualFiveVals : Series<string,float>
+          TTable : Frame<'b,string>
+          RSquared : float
+          AdjRSquared : float
+        }      
+
+    val summary : t<'a,'b> -> Summary.t<'b> when 'b : comparison
+  
+  val multiDim : 'b seq -> 'b -> 'b option -> Frame<'a,'b> -> Fit.t<'a,'b> when 'b : comparison
+  val simple : 'b -> 'b -> 'b option -> Frame<'a,'b> -> Fit.t<'a,'b> when 'b : comparison
