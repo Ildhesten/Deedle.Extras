70cae65dedc9980e5a073a1fb699d1b3fdd7224d
Ildhesten <noneofyour@business.com>
2019-02-19 23:47:34 -0500

    Added documentation and shuffled some methods around.

--- Deedle.Extras/Chart.fs
+++ Deedle.Extras/Chart.fs
@@ -30,11 +30,6 @@ type Chart() =
     let mappedNames = Seq.map Some names
     Chart.Line (s, mappedNames)
 
-  static member Line(df : Frame<'a,'b>, columnKeys : 'b seq) =
-    let names = columnKeys |> Seq.map (sprintf "%A") |> Seq.map Some
-    let series = columnKeys |> Seq.map (fun k -> df.[k])
-    Chart.Line(series, names)
-
   static member Scatter (s : Series<'a,'b> seq, names : string option seq) =
     if Seq.length s <> Seq.length names then
       failwith "Sequence of data series must of same length as the names"
@@ -48,22 +43,53 @@ type Chart() =
     let mappedNames = Seq.map Some names
     Chart.Scatter (s, mappedNames)
 
-  static member Scatter (df : Frame<'a,'b>, columnKeys : 'b seq) =
-    let names = columnKeys |> Seq.map (sprintf "%A") |> Seq.map Some
-    let series = columnKeys |> Seq.map (fun k -> df.[k])
-    Chart.Scatter(series, names)
-
+/// <summary>
+/// This module contains helper methods for plotting data from data frames.
+/// </summary>
 type Frame() =
+
+  /// <summary>
+  /// Plots all columns in the data frame as line plots.
+  /// specified in the in the column key sequence.
+  /// </summary>
+  /// <param name="df">The data frame to plot.</param>
+  /// <param name="columnKeys">The column keys you want to plot.</param>
+  static member LineColumnKeys(df : Frame<'a,'b>, columnKeys : 'b seq) =
+    let names = 
+      columnKeys 
+      |> Seq.map (sprintf "%A") 
+      |> Seq.map Some
+    let series = 
+      columnKeys 
+      |> Seq.map (fun k -> df.[k])
+    Chart.Line(series, names)
+  
+  /// <summary>
+  /// Plots all columns in the data frame as scatter plots.
+  /// specified in the in the column key sequence.
+  /// </summary>
+  /// <param name="df">The data frame to plot.</param>
+  /// <param name="columnKeys">The column keys you want to plot.</param>
+  static member ScatterColumnKeys(df : Frame<'a,'b>, columnKeys : 'b seq) =
+    let names = 
+      columnKeys 
+      |> Seq.map (sprintf "%A") 
+      |> Seq.map Some
+    let series = 
+      columnKeys 
+      |> Seq.map (fun k -> df.[k])
+    Chart.Scatter(series, names)    
+
+  /// <summary>
+  /// Plots all columns in the data frame as scatter plots.
+  /// </summary>
+  /// <param name="df">The data frame to plot.</param>
+  static member Scatter (df : Frame<'a,'b>) =
+    Frame.ScatterColumnKeys(df, df.ColumnKeys)
+
   /// <summary>
   /// Plots all columns in the data frame as line plots.
   /// </summary>
   /// <param name="df">The data frame to plot.</param>
   static member Line (df : Frame<'a,'b>) =
-    let columnNames = 
-      df.ColumnKeys 
-      |> Seq.map (sprintf "%A") 
-      |> Seq.map Some
-    let series =
-      df.ColumnKeys
-      |> Seq.map (fun k -> df.[k])
-    Chart.Line(series, columnNames)
+    Frame.LineColumnKeys (df, df.ColumnKeys)
