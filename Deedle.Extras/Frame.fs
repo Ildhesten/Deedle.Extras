6db78aa4899c20c1e092940fe2d2294b16f51f3d
Ildhesten <noneofyour@business.com>
2019-04-05 10:30:26 +0200

    Added extra utility function to filter frame rows by key only

---
+++ Deedle.Extras/Frame.fs
@@ -0,0 +1,13 @@
+﻿namespace Deedle.Extras
+
+module Frame =
+  open Deedle
+
+  /// <summary>
+  /// Filters the rows in the frame, if the row key matches the predicate.
+  /// </summary>
+  /// <param name="predicate">The predicate to filter the row keys.</param>
+  /// <param name="frame">The frame to apply the predicate to.</param>
+  let filterRowsByKey predicate frame =
+    Frame.filterRows (fun key _ -> predicate key) frame
+
---
+++ Deedle.Extras/Frame.fsi
@@ -0,0 +1,6 @@
+﻿namespace Deedle.Extras
+
+module Frame =
+  open Deedle
+
+  val filterRowsByKey : ('a -> bool) -> Frame<'a,'b> -> Frame<'a,'b>
